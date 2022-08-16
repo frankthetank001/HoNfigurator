2cece1a225e46d3befe6fb73b2eb87ea73294d5e
dooshbagzz <89614057+dooshbagzz@users.noreply.github.com>
2022-08-16 23:03:28 +1200

    Further tidying of script
    Removed excess lines
    Added many command outputs to verbose output, so you can still view output of the script in full by adding -verbose when running

--- HoNfigurator-Install.ps1
+++ HoNfigurator-Install.ps1
@@ -1,15 +1,15 @@
 ## Install Chocolatey package manager ##
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) 2>&1 | Write-Verbose
 
 ## Install required software with chocolatey - this will also install the dependencies for these programs ##
-choco install python3 -y  ## Python 3 - to run the HoNfigurator launcher install
-choco install git -y ## Github Cli - clone the required repos
-choco install nssm -y ## Non-Sucking Service Manager - for automating server restarts
-refreshenv
+Write-Output "Installaing dependencies from Chocolatey"
+choco install python3 -y 2>&1 | Write-Verbose  ## Python 3 - to run the HoNfigurator launcher install
+choco install git -y 2>&1 | Write-Verbose ## Github Cli - clone the required repos
+choco install nssm -y 2>&1 | Write-Verbose ## Non-Sucking Service Manager - for automating server restarts
+refreshenv 2>&1 | Write-Verbose
 
 ## Download HoN client ##
-Write-Output "-Downloading HoN Client to current directory
-Please be patient - do NOT cancel"
+Write-Output "-Downloading HoN Client to current directory. Please be patient - do NOT cancel"
 $URL="https://store3.gofile.io/download/direct/a254d73c-9fe5-489f-85e5-92ac4bc6b084/HoN-Client-x64-CLEAN.zip"
 $ZIP="HoN-Client-x64.zip"
 $progressPreference = 'silentlyContinue'
@@ -17,22 +17,23 @@ Invoke-WebRequest -URI $URL -OutFile $ZIP
 
 ## Extract HoN Client ##
 Write-Output "-Extracting HoN Client to current directory"
-Expand-Archive -Path $ZIP -DestinationPath $pwd
+Expand-Archive -Path $ZIP -DestinationPath $pwd 2>&1 | Write-Verbose
 rm $ZIP
-Write-Output "Cloning server binaries from external github"
 
 ## Clone the Server Binaries and copy to HoN directory ##
+Write-Output "Cloning server binaries from external github"
 $WS=".\wasserver\"
 $HON=".\HoN-Client-x64-CLEAN\"
-git clone https://github.com/wasserver/wasserver
+git clone https://github.com/wasserver/wasserver 2>&1 | Write-Verbose
 cp $WS\hon_x64.exe, $WS\k2_x64.dll -Destination "$HON"
 cp $WS\cgame_x64.dll, $WS\game_shared_x64.dll, $WS\game_x64.dll -Destination "$HON\game"
 rm $WS -r -force
 
 ## Clone HoNfigurator files ##
+Write-Output "Cloning HoNfigurator files"
 $hondir=Get-Location
-git clone https://github.com/frankthetank001/HoNfigurator
-pip install -r .\HoNfigurator\dependencies\requirements.txt
+git clone https://github.com/frankthetank001/HoNfigurator 2>&1 | Write-Verbose
+pip install -r .\HoNfigurator\dependencies\requirements.txt 2>&1 | Write-Verbose
 ac -Path .\HoNfigurator\config\default_config.ini -Value "
 hon_directory = $hondir\HoN-Client-x64-CLEAN\"
 py .\HoNfigurator\honfigurator.py
