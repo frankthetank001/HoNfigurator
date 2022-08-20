8b070312fa4ca7834fac7de75d32ba4a386a982b
frankthetank001 <82205454+frankthetank001@users.noreply.github.com>
2022-08-20 23:55:48 +1000

    Update honfigurator.py

--- honfigurator.py
+++ honfigurator.py
@@ -233,8 +233,7 @@ class initialise():
                 print(f"Repository: {selected_branch}\nUpdate Status: {output.stdout}")
                 guilog.insert(END,f"Repository: {selected_branch}\nUpdate Status: {output.stdout}")
                 #os.execv(sys.argv[0], sys.argv)
-                python = sys.executable
-                os.execl(python, python, * sys.argv)
+                os.execv(__file__, sys.argv)
                 return output.returncode
             else:
                 print(f"Repository: {selected_branch}\nCheckout status: {checkout.stderr}")
