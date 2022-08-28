501418e3d187d52a33d305be062550559f1eb8ea
frankthetank001 <82205454+frankthetank001@users.noreply.github.com>
2022-08-28 21:54:26 +1000

    Update embedManagerCog.py
    updated kongor url for HTTPS
    replaced mediafire links with proper file sources

--- cogs/embedManagerCog.py
+++ cogs/embedManagerCog.py
@@ -45,9 +45,9 @@ svr_port = svr_port.strip('"')
 svr_proxy_enabled = server_data_dict['man_enableProxy']
 
 if processed_data_dict['master_server'] == "honmasterserver.com":
-    default_description = '[Hon Server Portal](https://discord.gg/k86ZcA3R8y)  |  [honmasterserver.com](https://honmasterserver.com)  |  [honclientfix.exe](https://www.mediafire.com/file/4xdih1yy54y4qah/HonClientFix.exe/file)'
+    default_description = '[Hon Server Portal](https://discord.gg/k86ZcA3R8y)  |  [honmasterserver.com](https://honmasterserver.com)  |  [honclientfix.exe](https://store5.gofile.io/download/direct/d8740070-c73e-43ec-9c00-2ea83c639e8d/HonClientFix-honmasterserver.exe)'
 elif processed_data_dict['master_server'] == "kongor.online:666":
-    default_description = '[Hon Server Portal](https://discord.gg/k86ZcA3R8y)  |  [Kongor Online](http://kongor.online:555)  |  [Kongor Client Fix](https://www.mediafire.com/file/ryeblwp3pgxfb4j/HonClientFix-kongor.exe/file)'
+    default_description = '[Hon Server Portal](https://discord.gg/k86ZcA3R8y)  |  [Kongor Online](https://kongor.online)  |  [Kongor Client Fix](https://store5.gofile.io/download/direct/d0f38bed-0d69-4d0e-a621-87e99d084b2a/HonClientFix-kongor.exe)'
 default_footer = "v{bot_version}  |  Games Played: {self.server_status['total_games_played']}  |  Last Restart: {self.last_restart}"
 
 os.environ["USERPROFILE"] = processed_data_dict['hon_home_dir']
