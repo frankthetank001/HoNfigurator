20ceaf936d0c32d460fe014a047974bf7270d7ee
frankthetank001 <82205454+frankthetank001@users.noreply.github.com>
2022-09-14 22:00:02 +1000

    Update honfigurator.py

--- honfigurator.py
+++ honfigurator.py
@@ -899,7 +899,7 @@ if is_admin():
                 try:
                     if 'Updating' in output.stdout or 'Switched to branch' in checkout.stderr:
                         if honfigurator.popup_bonus():
-                            os.execl(f"{sys.executable}", f"{os.path.abspath(__file__)}", *sys.argv)
+                            os.execl(sys.executable, "\""+os.path.abspath(__file__)"\"", *sys.argv)
                 except Exception as e: print(e)
                 return True
             else:
