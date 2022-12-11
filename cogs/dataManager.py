ddba834ba8760aea4a96753b46367739e96232d7
frankthetank001 <82205454+frankthetank001@users.noreply.github.com>
2022-12-11 16:12:23 +1100

    Update dataManager.py

--- cogs/dataManager.py
+++ cogs/dataManager.py
@@ -337,8 +337,12 @@ class mData():
             version_offset=88544
             hon_x64=open(file,'rb')
             hon_x64.seek(version_offset,1)
-            version=hon_x64.read(16)
-            return(version.decode('utf-16-le'))
+            version=hon_x64.read(18)
+            version=version.decode('utf-16-le')
+            if version.find("00"):
+                version=hon_x64.read(16)
+                version=version.decode('utf-16-le')
+            return(version)
         else:
             return ("pending version check") 
     def check_affinity(svr_id,core_assignment):
