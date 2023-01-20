e462243958c850d81ef28ef21990ee732c2bc5a1
frankthetank001 <82205454+frankthetank001@users.noreply.github.com>
2023-01-21 02:14:47 +1100

    Update honfigurator-fix.py

--- dependencies/honfigurator-fix.py
+++ dependencies/honfigurator-fix.py
@@ -1,6 +1,8 @@
 import subprocess
-output = subprocess.Popen(['git','pull'])
+output = subprocess.Popen(['git','pull'],stdout=subprocess.PIPE)
+result = output.stdout.read()
+result = result.decode()
 
-if "local changes" in output:
+if "local changes" in output.stdout:
     subprocess.Popen(['git','reset','--hard'])
     subprocess.Popen(['git','pull'])
